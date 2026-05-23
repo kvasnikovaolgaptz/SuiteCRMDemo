38eb0aa3f7aae78d11ee388b7d94b0ad4df150e2
Olga Kvasnikova <kvasnikovaolgaptz@gmail.com>
2026-05-23 11:57:24 +0300

    added gh-pages

--- src/main/java/pages/SaveAccountPage.java
+++
@@ -1,9 +0,0 @@
-package pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class SaveAccountPage extends BasePage {
-    public SaveAccountPage(WebDriver driver) {
-        super(driver);
-    }
-}
