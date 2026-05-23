219d9fe4779597912ceb9c6a6cb11e2cca5294c7
Olga Kvasnikova <kvasnikovaolgaptz@gmail.com>
2026-05-23 09:51:56 +0300

    added headless options

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -38,9 +39,12 @@ public class BaseTest {
             options.addArguments("--disable-notifications");
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-infobars");
+            options.addArguments("--headless"); // запуск тестов в режиме headless, для удаленного сервака
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            EdgeOptions options = new EdgeOptions(); // запуск тестов в режиме headless для второго браузера
+            options.addArguments("--headless");
+            driver = new EdgeDriver(options);
             driver.manage().window().maximize();
         }
 
